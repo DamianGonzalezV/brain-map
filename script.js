56141c8d3974c78d2ab7737d0b0a152e5c53aa46
damian.velazquez@uadec.edu.mx <damianvelazquez@uadec.edu.mx>
2025-04-21 06:32:31 -0600

    Displays card form

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const cardForm = document.querySelector(".card-form");
 const emptyStateBigIcon = document.querySelector(".ph-list-plus");
 
 const cardTitleInput = document.querySelector(".card-title-input");
+const cardTextArea = document.querySelector(".card-body-area");
 
 document.querySelector(".icon-wrapper").addEventListener("mouseover", () => {
   emptyStateBigIcon.classList.add("hide");
   document.querySelector(".ph-plus-circle-big").classList.add("show");
 });
 
-document.querySelector(".add-card-btn").addEventListener("click", () => {
+document.querySelector(".add-card-btn-home").addEventListener("click", () => {
   console.log("log");
   emptyMessageDiv.classList.add("main-hide");
   cardForm.classList.remove("main-hide");
@@ -24,6 +25,16 @@ cardTitleInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     const cardTitle = document.createElement("h3");
     cardTitle.textContent = cardTitleInput.value;
+    cardTitle.classList.add("cardTitle");
     cardTitleInput.replaceWith(cardTitle);
+  }
+});
+
+cardTextArea.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const cardText = document.createElement("p");
+    cardText.textContent = cardTextArea.value;
+    cardText.classList.add("cardText");
+    cardTextArea.replaceWith(cardText);
   }
 });
